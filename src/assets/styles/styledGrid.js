1845d7a1e28f2a451d39f0c1b0f0617b6343c56f
Artem <artyom.balamutyuk@mail.ru>
2019-01-03 00:20:53 +0300

    Add styled grid

---
+++ src/assets/styles/styledGrid.js
@@ -0,0 +1,34 @@
+import styled, { css } from 'styled-components';
+
+const columns = {
+  four: 1750,
+  three: 1400,
+  two: 1050,
+  one: 700
+};
+
+// iterate through the sizes and create a media template
+const mewMedia = Object.keys(columns).reduce((accumulator, label) => {
+  const emSize = columns[label] / 16;
+  accumulator[label] = (...args) => css`
+    @media (max-width: ${emSize}em) {
+      ${css(...args)};
+    }
+  `;
+  return accumulator;
+}, {});
+
+export const GridContainer = styled.div`
+  display: grid;
+  ${mewMedia.four`grid-template-columns: auto auto auto auto;`}
+  ${mewMedia.three`grid-template-columns: auto auto auto;`}
+  ${mewMedia.two`grid-template-columns: auto auto;`}
+  ${mewMedia.one`grid-template-columns: auto;`}
+  grid-gap: 20px;
+  background-color: transparent;
+  padding: 0px;
+`;
+
+export const GridItem = styled.div`
+  place-self: center;
+`;
