15ac9ec9d0243909b0511b2e8aeca0b7ebfa218d
Artem <artyom.balamutyuk@mail.ru>
2019-01-06 18:42:19 +0300

    Replace on react component, fix and add static propTypes and function

--- src/components/common/TestCard.jsx
+++ src/components/common/TestCard.jsx
@@ -1,45 +1,79 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { PropTypes } from 'prop-types';
+import { Link } from 'react-router-dom';
 import LinesEllipsis from 'react-lines-ellipsis';
 import { getTest } from '../../services/fakeTestService';
+import getPercentage from '../../utils/getPercentage';
 import {
+  Container,
   Card,
   CardHeader,
   CardBody,
   CardFooter,
   Rate,
   CardQuestion,
-  CardAuthor,
-  CardViews
+  CardAttr,
+  CardVerifyText,
+  CardVerifyIcon,
+  CardEdit
 } from '../../assets/styles/index';
 
-const TestCard = ({ id, isVerified, question, author, views, percent }) => (
-  <Card
-    onClick={() => {
-      console.log(getTest(id));
-    }}
-  >
-    <CardHeader verified={isVerified} />
-    <CardBody>
-      <CardQuestion>
-        <LinesEllipsis text={question} maxLine="4" ellipsis="..?" />
-      </CardQuestion>
-      <CardAuthor>{author}</CardAuthor>
-      <CardViews>{views}</CardViews>
-    </CardBody>
-    <CardFooter>
-      <Rate percent={percent} />
-    </CardFooter>
-  </Card>
-);
+class TestCard extends Component {
+  static mapToModelView(data) {
+    return {
+      id: data.id,
+      isVerified: data.verified,
+      question: data.question,
+      author: data.author,
+      views: data.views,
+      likes: data.likes,
+      dislikes: data.dislikes
+    };
+  }
 
-TestCard.propTypes = {
-  id: PropTypes.string.isRequired,
-  isVerified: PropTypes.bool.isRequired,
-  question: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
-  views: PropTypes.number.isRequired,
-  percent: PropTypes.string.isRequired
-};
+  static propTypes = {
+    id: PropTypes.string.isRequired,
+    isVerified: PropTypes.bool.isRequired,
+    question: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+    dislikes: PropTypes.number.isRequired
+  };
+
+  render() {
+    const { id, isVerified, question, author, views, likes, dislikes } = this.props;
+
+    return (
+      <Card
+        onClick={() => {
+          console.log(getTest(id));
+        }}
+      >
+        <CardHeader>
+          <Link to="/register">
+            <CardEdit icon="edit" />
+          </Link>
+          <Container none>
+            <CardVerifyText>{isVerified ? 'Verified' : 'Not verified'}</CardVerifyText>
+            <CardVerifyIcon icon={isVerified ? 'check' : 'times'} />
+          </Container>
+        </CardHeader>
+        <CardBody>
+          <CardQuestion>
+            <LinesEllipsis text={question} maxLine="4" ellipsis="..?" />
+          </CardQuestion>
+          {/* eslint-disable */}
+          <CardAttr>Author: {author}</CardAttr>
+          <CardAttr>Viwes: {views}</CardAttr>
+          {/* eslint-enable */}
+        </CardBody>
+        <CardFooter>
+          <Rate percent={getPercentage(likes, likes + dislikes)} />
+        </CardFooter>
+      </Card>
+    );
+  }
+}
 
 export default TestCard;
